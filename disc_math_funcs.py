9c41dfd6bf08f87445b254867460c298bab1e8f3
towles-osu <71574207+towles-osu@users.noreply.github.com>
2020-11-09 15:55:02 -0800

    Create disc_math_funcs.py

---
+++ disc_math_funcs.py
@@ -0,0 +1,10 @@
+#auth0r: Stew
+#This is some functions that are helpful for the discrete math class I'm taking
+
+def permut(n, k):
+  answer = factorial(n) / factorial(n-k)
+  return answer
+  
+def combin(n, k)
+  answer = factorial(n) / (factorial(n-k) * factorial(k))
+  return answer
