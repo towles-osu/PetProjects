91f4c978fdd97376441067cfc75921594aa2a1c0
towles-osu <towles@oregonstate.edu>
2020-11-09 16:04:43 -0800

    update

--- disc_math_funcs.py
+++ disc_math_funcs.py
@@ -3,14 +3,17 @@
 
 def permut(n, k):
   answer = 1
-  for i in range(n,k,-1):
+  for i in range(n,n-k,-1):
     answer = answer * i
   return answer
   
 def combin(n, k):
   answer = 1
-  for i in range(n, k, -1):
+  for i in range(n, n-k, -1):
     answer = answer * i
   for i_2 in range(k,0,-1):
     answer = answer / i_2
   return answer
+
+# print(permut(5,3))
+# print(combin(5,3))
