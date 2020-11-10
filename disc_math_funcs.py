2e8f2c8264fd622332c625e51f07a0e082540450
towles-osu <towles@oregonstate.edu>
2020-11-09 18:10:28 -0800

    update

--- disc_math_funcs.py
+++ disc_math_funcs.py
@@ -2,18 +2,22 @@
 #This is some functions that are helpful for the discrete math class I'm taking
 
 def permut(n, k):
+  """this calculates k-permutations of n objects"""
   answer = 1
   for i in range(n,n-k,-1):
     answer = answer * i
   return answer
   
 def combin(n, k):
+  """this calculates k-combinations of n objects"""
   answer = 1
   for i in range(n, n-k, -1):
     answer = answer * i
   for i_2 in range(k,0,-1):
     answer = answer / i_2
   return answer
+
+def find_bin_term(n,k):
 
 # print(permut(5,3))
 # print(combin(5,3))
---
+++ text_based_game.py
@@ -0,0 +1,5 @@
+# Author: Stew
+# Date: November 2020
+# Description: This is a fresh attempt at creating a text based game template
+# The goal is to have a program that reads information from text files to create a text based game
+# with custom attributes but standard basic template
